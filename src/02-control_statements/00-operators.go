5a88883aafa54a3ce4341713454137cd3adc1137
ratarca <ratarca.rtc@gmail.com>
2023-05-02 21:54:37 -0300

    add:control-statements:decisions

--- src/02-control_statements/00-operators.go
+++ src/02-control_statements/00-operators.go
@@ -1,15 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	/*
-			Math operators
+			Arithmetic
 		{sum:+, minus:- , multi:* , division: /, modules: % }
 	*/
+	fmt.Println("Arithmetic Operators")
 	var a int16 = 8
 	var b int16 = 4
-	hipotenusa := (a + b) * (1 / 2)
-	fmt.Println(hipotenusa)
+	calculus := ((a + b) * (a - b)) / b
 
+	fmt.Println(calculus)
+
+	/*
+			Relational
+		> ; >= ; < ; <= ; == ; !=
+	*/
+	fmt.Println("Relational Operators")
+	fmt.Println(a > b, b >= (a/2))
+	fmt.Println(a < b, b <= (a/2))
+	fmt.Println(a == b, b != (a/2))
+
+	/*
+			Logical
+		AND {&&}; OR {||}; NOT {!}
+	*/
+	fmt.Println("Logical Operators")
+	first_condition := (a > b) && (b > a)
+	var second_condition bool = (a > b) || (b > a)
+
+	fmt.Println(first_condition, second_condition)
 }
