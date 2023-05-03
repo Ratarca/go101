679c5baf413af3848744fa758ae59e08376735b8
ratarca <ratarca.rtc@gmail.com>
2023-05-03 07:30:16 -0300

    feat:new readme

--- src/02-control_statements/02-for_loop.go
+++ src/02-control_statements/02-for_loop.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var doTask int = 10
+	counter := 0
+
+	// For
+	fmt.Println("Task counter")
+	for counter < doTask {
+		fmt.Println(counter, doTask)
+		counter++
+	}
+
+	// For with condition
+	fmt.Println("Adult life counter")
+	var adult int = 21
+	for i := 0; i < adult; i++ {
+		fmt.Println(i)
+	}
 }
