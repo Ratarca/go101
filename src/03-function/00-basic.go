d8c7fe4324949573b5ae31867dfeea37ba35c92a
ratarca <ratarca.rtc@gmail.com>
2023-05-05 16:27:27 -0300

    functions

--- src/03-function/00-basic.go
+++ src/03-function/00-basic.go
@@ -13,6 +13,12 @@ func calculus(a, b int) (int, string) {
 	return sum, "ANOTHER RETURN"
 }
 
+// Function with named return therefore your output has a variable name
+func somar(a, b int) (somatorio int) {
+	somatorio = a + b
+	return somatorio
+}
+
 func main() {
 	//
 	l3 := hipotenuse(1, 3)
@@ -28,5 +34,7 @@ func main() {
 	//
 	results, _ := calculus(10, 5)
 	fmt.Println(results)
+
+	//
 
 }
--- src/03-function/01-arguments.go
+++ src/03-function/01-arguments.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+// variadic FUnctions : Functions with N arguments : Pack is a
+func somar(numbers ...int) {
+	fmt.Println(numbers)
+
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+
+	return total
+}
+
+func main() {
+	somar(1, 2, 3, 4, 5, 6, 7, 8, 10)
 }
