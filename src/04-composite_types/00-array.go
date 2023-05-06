ee0a3b42b49d0e07f9b3e6e2681db1419dee2825
ratarca <ratarca.rtc@gmail.com>
2023-05-06 14:49:22 -0300

    change:functions and composite_types

--- src/04-composite_types/00-array.go
+++ src/04-composite_types/00-array.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+// Simple Array : Has a fixed size and same type
+var simple_array [5]int
+
+// Array Methods
+
 func main() {
+	fmt.Println(simple_array)
+
+	declared_array := [4]int{2, 3, 5, 7}
+	fmt.Println(declared_array)
+
+	simple_array[0] = 10
+	fmt.Println(simple_array)
 
 }
